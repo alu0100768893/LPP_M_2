885a1d8f3d789a4a58b0cdf5270267592b36040c
Miguel Ángel Delgado Hernández <alu0100738896@ull.edu.es>
2014-10-17 13:55:26 +0100

    Añadido division

--- lib/complejo2.rb
+++ lib/complejo2.rb
@@ -4,10 +4,15 @@ class Complejos
 
 		if comp.class == Complejos
 			Complejos.new((@re*comp.re) - (@im*comp.im), 
-					(@re*comp.im) + (@im*comp.re))
+							(@re*comp.im) + (@im*comp.re))
 		else
 			Complejos.new(@re*comp.to_i, @im*comp.to_i)
 		end
+	end
+
+	def / (comp)
+		Complejos.new((@re*comp.re+@im*comp.im) / (comp.re**2+comp.im**2),
+						(@im*comp.re-@re*comp.im) / (comp.re**2+comp.im**2))
 	end
 	
 
